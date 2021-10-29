230d739bf52fd4d75158dc6a8487ee6052621bab
rocha <gabrielrochadossantos1999@gmail.com>
2021-10-29 20:52:23 -0300

    classes de dominios iniciadas

--- src/main/java/com/rocha/bookstore/domain/Categoria.java
+++ src/main/java/com/rocha/bookstore/domain/Categoria.java
@@ -1,4 +1,77 @@
 package com.rocha.bookstore.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Categoria {
+
+    private Integer id;
+    private String texto;
+    private String descricao;
+
+    private List<Livro> livros = new ArrayList<>();
+
+    public Categoria() {
+        super();
+    }
+
+    public Categoria(Integer id, String texto, String descricao) {
+        super();
+        this.id = id;
+        this.texto = texto;
+        this.descricao = descricao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(List<Livro> livros) {
+        this.livros = livros;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        if (id == null) {
+            if (categoria.id != null) return false;
+        } else if (!id.equals(categoria.id)) return false;
+        return true;
+    }
+
+
 }
