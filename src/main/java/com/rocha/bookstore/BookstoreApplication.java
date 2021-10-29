060d70ab009aaed53bd3e52f8f4520f7ade10ad4
rocha <gabrielrochadossantos1999@gmail.com>
2021-10-29 20:07:04 -0300

    commit inicial

---
+++ src/main/java/com/rocha/bookstore/BookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.rocha.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+}
