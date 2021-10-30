41dd5daa9a71ddf48e11954a19471a814a0f6725
rocha <gabrielrochadossantos1999@gmail.com>
2021-10-29 21:13:49 -0300

    repositories implementadas

---
+++ src/main/java/com/rocha/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.rocha.bookstore.repositories;
+
+import com.rocha.bookstore.domain.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+}
