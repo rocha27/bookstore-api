f548dc8e3e14cd0bc78b9c55dda4d3ce3e17936b
rocha <gabrielrochadossantos1999@gmail.com>
2021-10-30 03:56:07 -0300

    perfil de teste implementada

--- src/main/java/com/rocha/bookstore/BookstoreApplication.java
+++ src/main/java/com/rocha/bookstore/BookstoreApplication.java
@@ -1,38 +1,12 @@
 package com.rocha.bookstore;
 
-import com.rocha.bookstore.domain.Categoria;
-import com.rocha.bookstore.domain.Livro;
-import com.rocha.bookstore.repositories.CategoriaRepository;
-import com.rocha.bookstore.repositories.LivroRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-
 @SpringBootApplication
-public class BookstoreApplication implements CommandLineRunner {
+public class BookstoreApplication{
 
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-
-	@Autowired
-	private LivroRepository livroRepository;
-
-	public static void main(String[] args) {
-		SpringApplication.run(BookstoreApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Categoria cat1 = new Categoria(null, "Informatica", "Livros de TI");
-
-		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "lorem ipsum", cat1);
-
-		cat1.getLivros().addAll(Arrays.asList(l1));
-
-		this.categoriaRepository.saveAll(Arrays.asList(cat1));
-		this.livroRepository.saveAll(Arrays.asList(l1));
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreApplication.class, args);
+    }
 }
